cd87fcd2059357c4b5a017dccbace11504b9f96f
Dmytro Frolov1 <Dmytro_Frolov1@epam.com>
2019-09-14 17:09:57 +0300

    fix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/<name-of-app>'));
 app.get('/*', function(req,res) {
-
+  res.header('Content-Security-Policy', 'default-src \'unsafe-eval\' *; script-src-elem *; style-src \'unsafe-inline\' *; object-src \'self\' *; img-src \'self\' *; media-src \'self\' *; frame-src \'self\' *; font-src \'self\' *; connect-src *;');
   res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 });
 
