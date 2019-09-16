0662fbe508cb95bccfbc641b621db00d77b26a85
Dmytro Frolov1 <Dmytro_Frolov1@epam.com>
2019-09-16 18:04:11 +0300

    sort fixes

--- src/app/pages/player-character-page/components/inventory/inventory.component.ts
+++ src/app/pages/player-character-page/components/inventory/inventory.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {Store} from '@ngrx/store';
 import {StoreInterface} from '../../../../store/store.model';
 import {Item} from '../../../../models/equipment/item/item.model';
@@ -8,15 +8,16 @@ import {MatSort, MatTableDataSource} from '@angular/material';
 
 export interface PlayerEquipmentView extends Item {
   count: number;
+  name: string;
 }
-const equipmentView: PlayerEquipmentView[] = [];
+let equipmentView: PlayerEquipmentView[] = [];
 
 @Component({
   selector: 'app-inventory',
   templateUrl: './inventory.component.html',
   styleUrls: ['./inventory.component.scss']
 })
-export class InventoryComponent implements OnInit {
+export class InventoryComponent implements OnInit, OnDestroy {
   public currentUser: Character;
   searchingItem: string;
   displayedColumns: string[] = ['name', 'count', 'weight', 'cost'];
@@ -37,10 +38,13 @@ export class InventoryComponent implements OnInit {
   ngOnInit() {
     this.currentUser.equipment.forEach((characterItem) => {
       this.db.getItem(characterItem.value).subscribe((element: Item) => {
-        equipmentView.push({...element, count: characterItem.count});
+        equipmentView.push({...element, count: characterItem.count, name: element.title.ru.toString()});
         this.dataSource.sort = this.sort;
       });
     });
+  }
+  ngOnDestroy(): void {
+    equipmentView = [];
   }
 
   addItems() {
