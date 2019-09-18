72ab8202624c24e5b097be909442c31907cf4c90
dmitrolov <dmitrolov@gmail.com>
2019-09-18 22:37:08 +0300

    environment for heroku fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCBJLGUwm3Gudef3OmMpTr4_kuAKojdHKI',
+    authDomain: 'dnd-master-tools.firebaseapp.com',
+    databaseURL: 'https://dnd-master-tools.firebaseio.com',
+    projectId: 'dnd-master-tools',
+    storageBucket: '',
+    messagingSenderId: '338033505662',
+    appId: '1:338033505662:web:ded866bbf17722c7'
+  },
 };
